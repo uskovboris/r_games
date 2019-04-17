fbe3f11188764017f4b86e90cf27e853ebf54128
Boris Uskov <uskov_b@mail.ru>
2019-04-17 07:26:33 +0300

    add comment

--- data_structures/all_any_funcs.R
+++ data_structures/all_any_funcs.R
@@ -1,3 +1,11 @@
+# Suppose that R executes the following:
+#   > any(x > 5)
+# It first evaluates x > 5:
+#   (FALSE, FALSE, FALSE, FALSE, FALSE)
+# We use any() function - that reports whether any
+# of those values are TRUE while all() function works
+# and reports if all the values are TRUE.
+
 x <- 1:10
 print(all(x>5))
 print(any(x>5))
